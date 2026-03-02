b62d8a8969251ce33a19bd131318f0b708214205
harshsksh <hr392002@gmail.com>
2026-03-02 09:50:54 +0530

    Initialize core middleware and API error/response handling utilities.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch( (error) => console.log("Error in connecting MongoDB", error))
+
 
