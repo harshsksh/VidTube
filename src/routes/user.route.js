d7b21686433b21c15407020a6fc34d5a4def67de
harshsksh <hr392002@gmail.com>
2026-03-06 08:46:43 +0530

    feat: add user authentication and profile management routes with file upload and JWT protection

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshToken } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshToken, changePassword, getCurrentUser, updateProfile, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +25,19 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshToken)
+
+router.route("/change-password").post(verifyJWT, changePassword)
+
+router.route("/get-current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-profile").patch(verifyJWT, updateProfile)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/get-user-channel-profile").get(verifyJWT, getUserChannelProfile)
+
+router.route("/get-watch-history").get(verifyJWT, getWatchHistory)
 
 export default router;
